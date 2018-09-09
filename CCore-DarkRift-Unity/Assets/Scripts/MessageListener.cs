816724879a87e9c58fb681a6fa9b3763f889e550
Nils <its.just.nils@gmail.com>
2018-09-09 09:55:13 +0200

    add unity project

---
+++ CCore-DarkRift-Unity/Assets/Scripts/MessageListener.cs
@@ -0,0 +1,18 @@
+﻿using DarkRift.Client;
+using DarkRift.Client.Unity;
+using UnityEngine;
+
+public class MessageListener : MonoBehaviour
+{
+	[SerializeField] private UnityClient client;
+	
+	void Awake() 
+	{
+		client.MessageReceived += ClientOnMessageReceived;
+	}
+
+	private void ClientOnMessageReceived(object sender, MessageReceivedEventArgs e)
+	{
+		Debug.Log(e.Tag);
+	}
+}
